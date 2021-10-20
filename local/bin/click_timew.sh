bf8edef11c0c3f27d769b7d2cc15ee534aac4432
Frederick Price <rprice@pricemail.ca>
2021-10-20 14:16:06 -0400

    Send message when starting or stopping TimeWarrior

--- local/bin/click_timew.sh
+++ local/bin/click_timew.sh
@@ -3,6 +3,6 @@
 response=$(timew | awk '$1 == "Current" {print "Running"} $1 == "There" {print "Idle"}')
 
 case $response in
-    "Running" ) timew stop :quiet ;;
-    "Idle" ) timew continue :quiet ;;
+    "Running" ) timew stop :quiet; notify-send "Stopping TimeWarrior tracking." ;;
+    "Idle" ) timew continue :quiet; notify-send "Starting TimeWarrior tracking." ;;
 esac
