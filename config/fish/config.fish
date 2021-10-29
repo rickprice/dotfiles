677801e98452a2115f42aa6c589f4056ac2c61be
Frederick Price <rprice@pricemail.ca>
2021-10-29 10:29:27 -0400

    Checking in some changes

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,3 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
 end
-# -- START ACTIVESTATE DEFAULT RUNTIME ENVIRONMENT
-set -xg PATH "/home/fprice/.cache/activestate/bin:$PATH"
-# -- STOP ACTIVESTATE DEFAULT RUNTIME ENVIRONMENT
