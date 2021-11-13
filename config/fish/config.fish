0b79d12da238458617eebe06dddbc03d9b60f8d5
Frederick Price <rprice@pricemail.ca>
2021-11-12 21:17:27 -0500

    Adding State tool setup

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,3 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
 end
+# -- START ACTIVESTATE INSTALLATION
+set -xg PATH "/home/fprice/.local/ActiveState/StateTool/release:$PATH"
+# -- STOP ACTIVESTATE INSTALLATION
+# -- START ACTIVESTATE DEFAULT RUNTIME ENVIRONMENT
+set -xg PATH "/home/fprice/.cache/activestate/bin:$PATH"
+# -- STOP ACTIVESTATE DEFAULT RUNTIME ENVIRONMENT
