395060d01a79c93a00f2151091447ea1c89df5cd
Frederick Price <rprice@pricemail.ca>
2022-01-26 15:58:34 -0500

    Change config.fish so that fzf display of variables is on <CA>V instead of <C>V

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
+    fzf_configure_bindings --variables=\e\cV
 end
 # -- START ACTIVESTATE INSTALLATION
 set -xg PATH "/home/fprice/.local/ActiveState/StateTool/release:$PATH"
