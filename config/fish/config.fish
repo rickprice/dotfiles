33792ee4460dbadb704dd2ef7cc6b477de0a841c
Frederick Price <rprice@pricemail.ca>
2022-01-27 14:29:26 -0500

    Add starship prompt for Fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
     fzf_configure_bindings --variables=\e\cV
+    starship init fish | source
 end
 # -- START ACTIVESTATE INSTALLATION
 set -xg PATH "/home/fprice/.local/ActiveState/StateTool/release:$PATH"
