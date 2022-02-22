4ad4fc9593ac89422dbce77556ccc3afbe2f98f5
Frederick Price <rprice@pricemail.ca>
2022-02-22 15:26:51 -0500

    Trying to get Wezterm to handle cut/paste better

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,31 +1,63 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 return {
-  enable_tab_bar=false,
+  enable_tab_bar = false,
   -- color_scheme="Adventure",
   -- color_scheme="Jellybeans",
   -- color_scheme="BuiltinDark",
 
   -- Based off Jellybeans, but with pure black background
-      colors = {
-        foreground = "#dedede",
-        background = "black",
-        cursor_bg = "#ffa560",
-        cursor_border = "#ffa560",
-        cursor_fg = "#ffffff",
-        selection_bg = "#474e91",
-        selection_fg = "#f4f4f4",
+  colors = {
+    foreground = "#dedede",
+    background = "black",
+    cursor_bg = "#ffa560",
+    cursor_border = "#ffa560",
+    cursor_fg = "#ffffff",
+    selection_bg = "#474e91",
+    selection_fg = "#f4f4f4",
 
-        ansi = {"#929292","#e27373","#94b979","#ffba7b","#97bedc","#e1c0fa","#00988e","#dedede"},
-        brights = {"#bdbdbd","#ffa1a1","#bddeab","#ffdca0","#b1d8f6","#fbdaff","#1ab2a8","#ffffff"},
-        },
+    ansi = { "#929292", "#e27373", "#94b979", "#ffba7b", "#97bedc", "#e1c0fa", "#00988e", "#dedede" },
+    brights = { "#bdbdbd", "#ffa1a1", "#bddeab", "#ffdca0", "#b1d8f6", "#fbdaff", "#1ab2a8", "#ffffff" },
+  },
 
-    font = wezterm.font_with_fallback({"Iosevka","Source Code Pro","Source Code Variable","PowerlineSymbols"}),
-    font_size = 11.0,
-    -- dpi = 96.0,
-    bold_brightens_ansi_colors = true,
-    -- font_dirs = {"/usr/share/fonts/TTF"},
-    font_shaper = "Harfbuzz",
-    harfbuzz_features = {"kern", "liga", "clig", "calt"},
-    scrollback_lines = 10000,
+  font = wezterm.font_with_fallback({ "Iosevka", "Source Code Pro", "Source Code Variable", "PowerlineSymbols" }),
+  font_size = 11.0,
+  -- dpi = 96.0,
+  bold_brightens_ansi_colors = true,
+  -- font_dirs = {"/usr/share/fonts/TTF"},
+  font_shaper = "Harfbuzz",
+  harfbuzz_features = { "kern", "liga", "clig", "calt" },
+  scrollback_lines = 10000,
+
+  -- keys = {
+  --   -- copy to clipboard and primary selection
+  --   { key = "C", mods = "SHIFT|ALT", action = wezterm.action({ CopyTo = "ClipboardAndPrimarySelection" }) },
+  --   -- paste from the clipboard
+  --   { key = "V", mods = "SHIFT|ALT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+  -- },
+  -- mouse_bindings = {
+  --   -- Bind 'Up' event of CTRL-Click to open hyperlinks
+  --   {
+  --     event = { Up = { streak = 1, button = "Left" } },
+  --     mods = "CTRL",
+  --     action = "OpenLinkAtMouseCursor",
+  --   },
+  --   -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
+  --   {
+  --     event = { Down = { streak = 1, button = "Left" } },
+  --     mods = "CTRL",
+  --     action = "Nop",
+  --   },
+  -- -- Bind 'Up' event of Double-Click to copy to clipboard and selection
+  -- {
+  --   event = { Up = { streak = 2, button = "Left" } },
+  --   mods = "",
+  --   action = "ClipboardAndPrimarySelection",
+  -- },
+  -- -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
+  -- {
+  --   event = { Down = { streak = 2, button = "Left" } },
+  --   mods = "",
+  --   action = "Nop",
+  -- },
+  -- },
 }
-
