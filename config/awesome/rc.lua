ff88ad0dc3b616f3ae11a7d82e0b87e346cb7480
Frederick Price <rprice@pricemail.ca>
2022-03-01 12:40:23 -0500

    Switch to a FrederickPrice theme based on default

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -39,7 +39,8 @@ end)
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+-- beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/FrederickPrice/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 -- terminal = "xterm"
