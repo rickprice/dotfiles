f6b81e14a0d3d7df36f0d5df1a55d635eff7108e
Frederick Price <rprice@pricemail.ca>
2022-03-01 13:30:20 -0500

    Fix window rules around Zoom and Slack

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -620,8 +620,8 @@ ruled.client.connect_signal("request::rules", function()
   --     rule       = { class = "Firefox"     },
   --     properties = { screen = 1, tag = "2" }
   -- }
-  ruled.client.append_rule({ rule = { class = "zoom" }, properties = { screen = 1, tag = "11" } })
-  ruled.client.append_rule({ rule = { class = "slack" }, properties = { screen = 2, tag = "12" } })
+  ruled.client.append_rule({ rule = { class = "zoom" }, properties = { screen = 2, tag = "11" } })
+  ruled.client.append_rule({ rule = { class = "slack" }, properties = { screen = 1, tag = "12" } })
   ruled.client.append_rule({ rule = { class = "discord" }, properties = { screen = 1, tag = "12" } })
 end)
 -- }}}
