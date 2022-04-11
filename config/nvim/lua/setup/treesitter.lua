c0907668e367e0e814d230f8a1dcd0c434c7f5af
Frederick Price <rprice@pricemail.ca>
2022-04-11 15:50:36 -0400

    Get rid of warning

--- config/nvim/lua/setup/treesitter.lua
+++ config/nvim/lua/setup/treesitter.lua
@@ -25,4 +25,4 @@ require("nvim-treesitter.configs").setup({
 })
 -- Setup treesitter
 local ts = require("nvim-treesitter.configs")
-ts.setup({ ensure_installed = "maintained", highlight = { enable = true } })
+ts.setup({ ensure_installed = "all", highlight = { enable = true } })
