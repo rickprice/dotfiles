b76ff27be002cb4792c9c0b18937a3ba45fedcf7
Frederick Price <rprice@pricemail.ca>
2022-07-13 21:55:05 -0400

    Add githubreviews alias, and add +x to zoxide.fish

---
+++ config/fish/conf.d/GitHubReviews.fish
@@ -0,0 +1,1 @@
+alias ghreviews="gh search prs --state=open --review-requested=@me --json=repository,number,createdAt --sort=created --order=asc --template='{{range .}}{{tablerow (printf \"https://github.com/%s/pull/%v %s\" .repository.nameWithOwner .number .createdAt | autocolor \"green\") .title}}{{end}}'"
