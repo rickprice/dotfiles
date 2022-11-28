d51007b865c766578023ffedbc6c5947b4dcbb63
Frederick Price <rprice@pricemail.ca>
2022-11-28 13:49:39 -0500

    Latest changes, nightfox, fonts

--- config/nvim/lua/setup/nightfox.lua
+++ config/nvim/lua/setup/nightfox.lua
@@ -1,4 +1,6 @@
 local nightfox = require("nightfox")
-nightfox.setup({ options={transparent = true }})
+-- nightfox.setup({ options={transparent = true }})
+nightfox.setup({ options={}})
 
+-- vim.cmd([[colorscheme nightfox]]) -- Put your favorite colorscheme here
 vim.cmd([[colorscheme carbonfox]]) -- Put your favorite colorscheme here
