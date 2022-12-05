26d78d4bd50e3f75c2e02bcc36851a4501954bc9
Frederick Price <rprice@pricemail.ca>
2022-12-05 16:43:53 -0500

    Script to copy out configuration to flash drive

---
+++ local/bin/Export_Secrets_And_Config.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+# Fail script if any command fails
+set -e
+
+## keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+## echo an error message before exiting
+trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
+
+EXPORT_DIRECTORY=/run/media/fprice/KINGSTON/FPRICE_Secrets_And_Config
+
+echo "Exporting to:" $EXPORT_DIRECTORY
+
+mkdir -p $EXPORT_DIRECTORY
+
+# SSH
+cp -Rp ~/.ssh $EXPORT_DIRECTORY
+
+# GPG
+cp -Rp ~/.gnupg $EXPORT_DIRECTORY
+
+# Dotfiles
+DIR_TO_COPY=.dotfiles/.dotter
+mkdir -p $EXPORT_DIRECTORY/$DIR_TO_COPY
+cp -Rp ~/$DIR_TO_COPY/secrets.toml $EXPORT_DIRECTORY/$DIR_TO_COPY/.
+
+# Fish
+DIR_TO_COPY=.local/share/fish
+mkdir -p $EXPORT_DIRECTORY/$DIR_TO_COPY
+cp -Rp ~/$DIR_TO_COPY/fish_history $EXPORT_DIRECTORY/$DIR_TO_COPY/.
+
+# Python Version Requirements Cache
+DIR_TO_COPY=.PythonVersionRequirementsCache
+mkdir -p $EXPORT_DIRECTORY/$DIR_TO_COPY
+cp -Rp ~/$DIR_TO_COPY/* $EXPORT_DIRECTORY/$DIR_TO_COPY/.
+
+# WireGuard
+DIR_TO_COPY=WireGuard
+mkdir -p $EXPORT_DIRECTORY/$DIR_TO_COPY
+cp -Rp ~/$DIR_TO_COPY/* $EXPORT_DIRECTORY/$DIR_TO_COPY/.
+
