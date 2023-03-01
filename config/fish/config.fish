d4e9a35a82a087c6dfd04278ad481461799cf12e
Frederick Price <rprice@pricemail.ca>
2023-03-01 10:06:28 -0500

    AS configuration moved

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,15 +4,15 @@ if status is-interactive
     fzf_configure_bindings --variables=\e\cV
     starship init fish | source
 end
-# -- START ACTIVESTATE INSTALLATION
-set -xg PATH "/home/fprice/.local/ActiveState/StateTool/release/bin:$PATH"
-# -- STOP ACTIVESTATE INSTALLATION
-# -- START ACTIVESTATE DEFAULT RUNTIME ENVIRONMENT
-set -xg PATH "/home/fprice/.cache/activestate/bin:$PATH"
-# -- STOP ACTIVESTATE DEFAULT RUNTIME ENVIRONMENT
 # Haskell Cabal Setup
 set -xg PATH "/home/fprice/.cabal/bin:$PATH"
 # Haskell Cabal Setup
 # Haskell CHCUP Setup
 set -xg PATH "/home/fprice/.ghcup/bin:$PATH"
 # Haskell CHCUP Setup
+# -- START ACTIVESTATE INSTALLATION
+set -xg PATH "/home/fprice/.local/ActiveState/StateTool/release/bin:$PATH"
+# -- STOP ACTIVESTATE INSTALLATION
+# -- START ACTIVESTATE DEFAULT RUNTIME ENVIRONMENT
+set -xg PATH "/home/fprice/.cache/activestate/bin:$PATH"
+# -- STOP ACTIVESTATE DEFAULT RUNTIME ENVIRONMENT
