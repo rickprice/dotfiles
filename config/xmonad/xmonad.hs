2edbfd7ebc57e19832261b694e3ec5d6a442541c
Frederick Price <rprice@pricemail.ca>
2023-03-07 19:58:35 -0500

    Setup startup bits

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -1,7 +1,30 @@
 import XMonad
 import XMonad.Config.Desktop
+import XMonad.Hooks.ManageDocks
 
-main = xmonad desktopConfig
-    { terminal    = "wezterm"
-    , modMask     = mod4Mask
-    }
+myStartupHook = do
+  -- spawnOnce "exec feh --bg-scale /home/lucask/Pictures/wallpapers/redwood.jpg"
+  -- spawnOnce "picom -f --config /home/lucask/.config/picom/picom.conf &"
+  -- spawnOnce "feh --bg-scale ~/Pictures/wallpaper.jpg"
+  spawn "stalonetray &"
+  spawn "picom &"
+  spawn "dropbox &"
+  spawn "nm-applet &"
+  spawn "pamac-tray &"
+  spawn "blueman-tray &"
+  spawn "xfce4-power-manager &"
+  spawn "volumeicon &"
+  spawn "killall udiskie; udiskie --tray &"
+  spawn "slack &"
+  spawn "discord &"
+  spawn "autorandr mobile; autorandr docked"
+
+main=do 
+    xmonad $ docks def 
+        { 
+        terminal    = "wezterm"
+        , modMask     = mod4Mask
+        , layoutHook=avoidStruts $ layoutHook def
+        , manageHook=manageHook def <+> manageDocks
+        , startupHook = myStartupHook
+        } 
