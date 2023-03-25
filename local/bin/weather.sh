150376eeb07d58b56a5db48716cbd719ca3b6115
Frederick Price <fprice@pricemail.ca>
2023-03-25 17:53:23 -0400

    Add weather command

--- local/bin/weather.sh
+++ local/bin/weather.sh
@@ -1,3 +1,23 @@
-#!/usr/bin/env sh
+#!/bin/sh
+#
+# Gets and parses the forecast from the Weather Office RSS
+#
 
-curl wttr.in
+# Midland, Ontario, Canada
+URL='https://dd.weather.gc.ca/citypage_weather/xml/ON/s0000281_e.xml'
+
+TEMPERATURE_XPATH='//currentConditions/temperature'
+WIND_SPEED_XPATH='//currentConditions/wind/speed'
+WIND_DIRECTION_XPATH='//currentConditions/wind/direction'
+PRESSURE_XPATH='//currentConditions/pressure'
+
+curl -s $URL |
+	xml sel -t \
+		-o "Temp: " -v "$TEMPERATURE_XPATH" -v "$TEMPERATURE_XPATH/@units" \
+		-o " Wind: " \
+		-v "$WIND_DIRECTION_XPATH" -o " " \
+		-v "$WIND_SPEED_XPATH" -v "$WIND_SPEED_XPATH/@units" \
+		-o " Pressure: " \
+		-v "$PRESSURE_XPATH" \
+		-v "$PRESSURE_XPATH/@units" -o " " \
+		-v "$PRESSURE_XPATH/@tendency"
