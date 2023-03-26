38bf2c31d5071f1f0090ec5d43b12cdb0fddebdc
Frederick Price <fprice@pricemail.ca>
2023-03-25 20:49:55 -0400

    Add the weather forecast

--- local/bin/weather.sh
+++ local/bin/weather.sh
@@ -10,14 +10,17 @@ TEMPERATURE_XPATH='//currentConditions/temperature'
 WIND_SPEED_XPATH='//currentConditions/wind/speed'
 WIND_DIRECTION_XPATH='//currentConditions/wind/direction'
 PRESSURE_XPATH='//currentConditions/pressure'
+FORECAST_XPATH='//hourlyForecastGroup/hourlyForecast[1]/condition'
 
 curl -s $URL |
 	xml sel -t \
-		-o "Temp: " -v "$TEMPERATURE_XPATH" -v "$TEMPERATURE_XPATH/@units" \
-		-o " Wind: " \
+		-o "T: " -v "$TEMPERATURE_XPATH" -v "$TEMPERATURE_XPATH/@units" \
+		-o " W: " \
 		-v "$WIND_DIRECTION_XPATH" -o " " \
 		-v "$WIND_SPEED_XPATH" -v "$WIND_SPEED_XPATH/@units" \
-		-o " Pressure: " \
+		-o " P: " \
 		-v "$PRESSURE_XPATH" \
 		-v "$PRESSURE_XPATH/@units" -o " " \
-		-v "$PRESSURE_XPATH/@tendency"
+		-v "$PRESSURE_XPATH/@tendency" \
+		-o " F: " \
+		-v "$FORECAST_XPATH"
