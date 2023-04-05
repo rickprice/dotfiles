ce2dd50124190e7ecfd7ce296df3a400745859e1
Frederick Price <fprice@pricemail.ca>
2023-04-05 14:08:02 -0400

    Downloads directory was moved to DropBox

--- local/bin/ProcessRawImages.sh
+++ local/bin/ProcessRawImages.sh
@@ -12,7 +12,7 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
 
 # Setup where everything goes
-DOWNLOADS_DIR=~/Downloads
+DOWNLOADS_DIR=~/Documents/Personal/Dropbox/FrederickDocuments/Downloads
 GOOGLE_TAKEOUT_DIR=./Takeout
 LOCATION_HISTORY_FILE_GPX="$GOOGLE_TAKEOUT_DIR/Location History/Location History.gpx"
 LOCATION_HISTORY_FILE_JSON="$GOOGLE_TAKEOUT_DIR/Location History/Records.json"
