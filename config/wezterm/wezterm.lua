3b6e08964808a16f9c9947145f43bb63de83b126
Frederick Price <fprice@pricemail.ca>
2023-07-25 13:26:48 -0400

    Make font smaller for Wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -20,7 +20,7 @@ return {
 	},
 
 	font = wezterm.font_with_fallback({ "Iosevka", "Source Code Pro", "Source Code Variable", "PowerlineSymbols" }),
-	font_size = 11.0,
+	font_size = 10.0,
 	-- dpi = 96.0,
 	bold_brightens_ansi_colors = true,
 	-- font_dirs = {"/usr/share/fonts/TTF"},
