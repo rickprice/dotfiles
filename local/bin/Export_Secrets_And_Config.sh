ed66edce68d8334ec9f455fc1bc068773bc19f64
Frederick Price <rprice@pricemail.ca>
2023-11-13 10:37:45 -0500

    Update backup command, still need to rename

--- local/bin/Export_Secrets_And_Config.sh
+++ local/bin/Export_Secrets_And_Config.sh
@@ -9,54 +9,45 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 ## echo an error message before exiting
 trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
 
-EXPORT_DIRECTORY=/run/media/fprice/KINGSTON/FPRICE_Secrets_And_Config
+# EXPORT_DIRECTORY=/run/media/fprice/KINGSTON/FPRICE_Secrets_And_Config
+EXPORT_DIRECTORY=/run/media/fprice/Backup
 
 echo "Exporting to:" $EXPORT_DIRECTORY
 
 mkdir -p $EXPORT_DIRECTORY
 
 # SSH
-cp -Rp ~/.ssh $EXPORT_DIRECTORY
+DIR_TO_COPY=~/.ssh
+rsync --archive $DIR_TO_COPY $EXPORT_DIRECTORY
 
 # GPG
-cp -Rp ~/.gnupg $EXPORT_DIRECTORY
+DIR_TO_COPY=~/.gnupg
+rsync --archive  $DIR_TO_COPY $EXPORT_DIRECTORY
 
 # Dotfiles
-DIR_TO_COPY=.dotfiles/.dotter
-mkdir -p $EXPORT_DIRECTORY/$DIR_TO_COPY
-cp -Rp ~/$DIR_TO_COPY/secrets.toml $EXPORT_DIRECTORY/$DIR_TO_COPY/.
+DIR_TO_COPY=~/.dotfiles/.dotter
+rsync --archive  $DIR_TO_COPY $EXPORT_DIRECTORY
 
 # Fish
-DIR_TO_COPY=.local/share/fish
-mkdir -p $EXPORT_DIRECTORY/$DIR_TO_COPY
-cp -Rp ~/$DIR_TO_COPY/fish_history $EXPORT_DIRECTORY/$DIR_TO_COPY/.
-
-# Python Version Requirements Cache
-DIR_TO_COPY=.PythonVersionRequirementsCache
-mkdir -p $EXPORT_DIRECTORY/$DIR_TO_COPY
-cp -Rp ~/$DIR_TO_COPY/* $EXPORT_DIRECTORY/$DIR_TO_COPY/.
+DIR_TO_COPY=~/.local/share/fish
+rsync --archive  $DIR_TO_COPY $EXPORT_DIRECTORY
 
 # WireGuard
-DIR_TO_COPY=WireGuard
-mkdir -p $EXPORT_DIRECTORY/$DIR_TO_COPY
-cp -Rp ~/$DIR_TO_COPY/* $EXPORT_DIRECTORY/$DIR_TO_COPY/.
-
-# ActiveState.yaml
-DIR_TO_COPY=TheHomeRepot
-mkdir -p $EXPORT_DIRECTORY/$DIR_TO_COPY
-cp -Rp ~/$DIR_TO_COPY/activestate.yaml.fish_fixes $EXPORT_DIRECTORY/$DIR_TO_COPY/.
+DIR_TO_COPY=~/WireGuard
+rsync --archive  $DIR_TO_COPY $EXPORT_DIRECTORY
 
 # Zoxzide
-DIR_TO_COPY=.local/share/zoxide
-mkdir -p $EXPORT_DIRECTORY/$DIR_TO_COPY
-cp -Rp ~/$DIR_TO_COPY/db.zo $EXPORT_DIRECTORY/$DIR_TO_COPY/.
+DIR_TO_COPY=~/.local/share/zoxide
+rsync --archive  $DIR_TO_COPY $EXPORT_DIRECTORY
 
 # Darktable
-DIR_TO_COPY=.config/darktable
-mkdir -p $EXPORT_DIRECTORY/$DIR_TO_COPY
-cp -Rp ~/$DIR_TO_COPY/* $EXPORT_DIRECTORY/$DIR_TO_COPY/.
+DIR_TO_COPY=~/.config/darktable
+rsync --archive  $DIR_TO_COPY $EXPORT_DIRECTORY
+DIR_TO_COPY=~/Documents/Personal/DarktablePersonal
+rsync --archive  $DIR_TO_COPY $EXPORT_DIRECTORY
+DIR_TO_COPY=~/Documents/Personal/DarktableCommercial
+rsync --archive  $DIR_TO_COPY $EXPORT_DIRECTORY
 
-# Pulse
-DIR_TO_COPY=etc/pulse
-mkdir -p $EXPORT_DIRECTORY/$DIR_TO_COPY
-cp -Rp /$DIR_TO_COPY/* $EXPORT_DIRECTORY/$DIR_TO_COPY/.
+# Dropbox
+DIR_TO_COPY=~/Documents/Personal/Dropbox
+rsync --archive  $DIR_TO_COPY $EXPORT_DIRECTORY
