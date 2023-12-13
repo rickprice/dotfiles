5e8e7deea7cf75c74ce32d0a263c25f224424ce5
Frederick Price <rprice@pricemail.ca>
2023-12-13 09:50:13 -0500

    Ungrab isn't used, and is now deprecated

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -41,7 +41,6 @@ import XMonad.StackSet qualified as W
 import XMonad.Util.EZConfig
 import XMonad.Util.Loggers
 import XMonad.Util.SpawnOnce
-import XMonad.Util.Ungrab
 
 -- ScratchPads
 import XMonad.Util.NamedScratchpad
