ab15f94f117079827463bc45c0675c240d64f282
Frederick Price <rprice@pricemail.ca>
2024-01-05 18:20:06 -0500

    Try to fix tabline in Neovim

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -241,24 +241,14 @@ return {
 		end,
 	},
 
-	-- the opts function can also be used to change the default opts:
 	{
 		"nvim-lualine/lualine.nvim",
-		event = "VeryLazy",
-		opts = function(_, opts)
-			table.insert(opts.sections.lualine_x, "😄")
-		end,
-	},
-
-	-- or you can return new options to override all the defaults
-	{
-		"nvim-lualine/lualine.nvim",
-		event = "VeryLazy",
-		opts = function()
-			return {
-				--[[add your custom lualine config here]]
-			}
-		end,
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			sections = {
+				lualine_a = { path = 4 },
+			},
+		},
 	},
 
 	-- use mini.starter instead of alpha
