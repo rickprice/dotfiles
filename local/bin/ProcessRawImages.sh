9efe5ea43f60f53e06acd46a7a035075dc16429b
Frederick Price <rprice@pricemail.ca>
2024-01-14 16:56:48 -0500

    Rewrite of the ProcessRawImages script to use the new google-location-history-converter executable

--- local/bin/ProcessRawImages.sh
+++ local/bin/ProcessRawImages.sh
@@ -1,10 +1,10 @@
-#! /usr/bin/sh
+#! /usr/bin/bash
 
 # Fail script if any command fails
 set -e
 
 # Echo commands
-# set -x
+set -x
 
 ## keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
@@ -13,102 +13,54 @@ trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
 
 # Setup where everything goes
 DOWNLOADS_DIR=~/Documents/Personal/Dropbox/FrederickDocuments/DropBoxDownloads
-GOOGLE_TAKEOUT_DIR=./Takeout
-LOCATION_HISTORY_FILE_GPX="$GOOGLE_TAKEOUT_DIR/Location History (Timeline)/Location History.gpx"
-LOCATION_HISTORY_FILE_JSON="$GOOGLE_TAKEOUT_DIR/Location History (Timeline)/Records.json"
-LOCATION_HISTORY_FILE_KML="$DOWNLOADS_DIR/GoogleTakeoutLocations.kml"
+LOCATION_HISTORY_FILE_TGZ=$DOWNLOADS_DIR/takeout-*
+LOCATION_HISTORY_FILE_KML=$DOWNLOADS_DIR/GoogleTakeoutLocations.kml
+DAYS_LOCATION_RECORDS_TO_USE=60
 WORKING_DIR=~/Documents/Personal/PhotosFromCamera
 
 # ARTIST="Tamara Price"
 ARTIST="Frederick Price"
 COMPANY="PricePixel Photography"
-# ARTIST="Joshua Murphy"
-# COMPANY=""
 
 cd "$WORKING_DIR"
 
-function process_photo_directory() {
-    local directory="$1"
-    local dropbox_location="$2"
-    local directory_location_data="${directory}_loc_data"
-    local directory_dropbox="${directory}_dropbox"
+function setup_google_location_data() {
+	echo Setup Google Takeout data
+	trash -f "$LOCATION_HISTORY_FILE_KML"
+	# Check if there is more than one takeout-* file, if so, then user needs to delete the old ones
+	if [[ $(find $DOWNLOADS_DIR -maxdepth 1 -name 'takeout-*' -printf c | wc -c) == "1" ]]; then
+		echo "...Starting Processing Location Data."
+		google-location-history-converter-exe --inputFile $LOCATION_HISTORY_FILE_TGZ --outputFile $LOCATION_HISTORY_FILE_KML --filterMoreThanDays=$DAYS_LOCATION_RECORDS_TO_USE
+		echo "...Finished Processing Location Data."
 
-    echo "Clear out directories if we can ${directory}"
-    trash -f "$directory_location_data"
-    [ -d $directory_dropbox ] && trash -f "$directory_dropbox"
-
-    echo "Copy pictures to be geolocated ${directory}"
-    mkdir -p "$directory_location_data"
-    cp -r $directory/* "$directory_location_data"
-
-    echo "Add location data $directory"
-    # [ "$(ls -A $directory)" ] && exiftool -geotag "$LOCATION_HISTORY_FILE_KML" '-geotime<${DateTimeOriginal}-04:00' . -api GeoMaxIntSecs=1800 -overwrite_original_in_place $directory_location_data/*
-    [ "$(ls -A $directory)" ] && exiftool -v5 -Copyright="All rights reserved. $COMPANY" -CopyrightNotice="All rights reserved. $COMPANY" -Rights="All rights reserved" -Artist="$ARTIST" -geotag "$LOCATION_HISTORY_FILE_KML" '-geotime<${DateTimeOriginal}-05:00' . -api GeoMaxIntSecs=1800 -overwrite_original_in_place $directory_location_data/*
-
-    # Sort the processed files in the location directory
-    pushd .
-    cd $directory_location_data
-    SortImageFilesInDirectory --extension CR2 --extension JPG
-    popd
-
-    # echo "Copy pictures to dropbox directory ${directory}"
-    # mkdir -p "$directory_dropbox"
-    # cp -R $directory_location_data/* "$directory_dropbox"
-
-    # # echo "Move pictures from [$directory_dropbox] to dropbox location [$dropbox_location] $directory"
-    # rclone move "${directory_dropbox}" "Dropbox:${dropbox_location}"
+	else
+		figlet Too many or too few takeout location zip files
+        echo "Location Checked: [" $LOCATION_HISTORY_FILE_TGZ "]"
+		exit -1
+	fi
 }
 
-echo Setup Google Takeout data
-trash -f "$GOOGLE_TAKEOUT_DIR"
-if [[ $(find $DOWNLOADS_DIR -maxdepth 1 -name 'takeout-*' -printf c | wc -c) == "1" ]]; then
-    # tar -xzf $DOWNLOADS_DIR/takeout-*
-    # location-history-json-converter -f kml -s 2022-01-01 "$LOCATION_HISTORY_FILE_JSON" "$LOCATION_HISTORY_FILE_KML"
-    # location-history-json-converter -f kml "$LOCATION_HISTORY_FILE_JSON" "$LOCATION_HISTORY_FILE_KML"
-    # location-history-json-converter -f gpxtracks -s 2021-11-01 "$LOCATION_HISTORY_FILE_JSON" "$LOCATION_HISTORY_FILE_GPX"
-    echo "...Starting Processing."
-    google-location-history-converter-exe --inputFile $DOWNLOADS_DIR/takeout-* --outputFile $DOWNLOADS_DIR/GoogleTakeoutLocations.kml --filterMoreThanDays 60 
-    echo "...Finished Processing."
+function process_photo_directory() {
+	local directory="$1"
+	local dropbox_location="$2"
+	local directory_location_data="${directory}_loc_data"
+	local directory_dropbox="${directory}_dropbox"
 
-else
-    figlet Too many or too few takeout location zip files
-    exit -1
-fi
+	echo "Clear out directories if we can ${directory}"
+	trash -f "$directory_location_data"
+	[ -d $directory_dropbox ] && trash -f "$directory_dropbox"
 
-process_photo_directory "$WORKING_DIR/CanonT7i"  "Pictures/CanonRawPhotos"
+	echo "Copy pictures to be geolocated ${directory}"
+	mkdir -p "$directory_location_data"
+	cp -r $directory/* "$directory_location_data"
+
+	echo "Add location data $directory"
+	exiftool -v5 -Copyright="All rights reserved. $COMPANY" -CopyrightNotice="All rights reserved. $COMPANY" -Rights="All rights reserved" -Artist="$ARTIST" -geotag "$LOCATION_HISTORY_FILE_KML" '-geotime<${DateTimeOriginal}-05:00' . -api GeoMaxIntSecs=1800 -overwrite_original_in_place $directory_location_data
+	echo "Organize Images $directory_location_data"
+	exiftool '-Directory<DateTimeOriginal' -d "$directory_location_data/%Y-%m-%d" $directory_location_data
+}
+
+
+setup_google_location_data
+process_photo_directory "$WORKING_DIR/CanonT7i" "Pictures/CanonRawPhotos"
 # process_photo_directory "$WORKING_DIR/CanonSX540HS"  "Pictures/CanonSX540HS"
-
-
-# Start terminal to watch the pipeline
-# terminal -e watch --interval 120 "ls --format=single-column $DROPBOX_UPLOAD_DIR $ORIGINAL_GOOGLE_PHOTOS_RAWS_UPLOAD_DIR $DROPBOX_UPLOAD_DIR_ERIC | wc --lines | figlet -k"
-
-# figlet Step 4: Copy original RAW files to staging
-# rsync --ignore-missing-args $ORIGINAL_RAWS_DIR/* $DROPBOX_UPLOAD_DIR
-# rsync --ignore-missing-args $ORIGINAL_CANON_POWERSHOT_DIR/* $DROPBOX_UPLOAD_DIR
-# rsync --ignore-missing-args $ORIGINAL_ERIC_CAMERA_DIR/* $DROPBOX_UPLOAD_DIR_ERIC
-# rsync --ignore-missing-args $ORIGINAL_GOOGLE_PHOTOS_RAWS/* $ORIGINAL_GOOGLE_PHOTOS_RAWS_UPLOAD_DIR
-
-# figlet Step 5: Geotag photos
-# [ "$(ls -A $DROPBOX_UPLOAD_DIR)" ] && exiftool -geotag "$LOCATION_HISTORY_FILE_KML" '-geotime<${DateTimeOriginal}-04:00' . -api GeoMaxIntSecs=1800 -overwrite_original_in_place $DROPBOX_UPLOAD_DIR/*
-# [ "$(ls -A $DROPBOX_UPLOAD_DIR_ERIC)" ] && exiftool -geotag "$LOCATION_HISTORY_FILE_KML" '-geotime<${DateTimeOriginal}-04:00' . -api GeoMaxIntSecs=1800 -overwrite_original_in_place $DROPBOX_UPLOAD_DIR_ERIC/*
-# [ "$(ls -A $ORIGINAL_GOOGLE_PHOTOS_RAWS_UPLOAD_DIR)" ] && exiftool -geotag "$LOCATION_HISTORY_FILE_KML" '-geotime<${DateTimeOriginal}-04:00' . -api GeoMaxIntSecs=1800 -overwrite_original_in_place $ORIGINAL_GOOGLE_PHOTOS_RAWS_UPLOAD_DIR/*
-
-# figlet Step 6: Convert photos going to Google to JPEG
-# Convert files in batch mode using default parameters
-# [ "$(ls -A $ORIGINAL_GOOGLE_PHOTOS_RAWS_UPLOAD_DIR)" ] && rawtherapee-cli -d -s -o $GOOGLE_PHOTOS_JPEG_UPLOAD_DIR -c $ORIGINAL_GOOGLE_PHOTOS_RAWS_UPLOAD_DIR
-
-# Remember we may need to enter password
-# figlet -k DropBox, You may need to enter passwword
-
-# figlet Step 7: Upload RAW files to Dropbox
-# rclone move "$DROPBOX_UPLOAD_DIR" Dropbox:Pictures/CanonRawPhotos
-# rclone move "$ORIGINAL_GOOGLE_PHOTOS_RAWS_UPLOAD_DIR" Dropbox:Pictures/CanonRawPhotos
-# rclone move "$DROPBOX_UPLOAD_DIR_ERIC" Dropbox:Pictures/EricCameraPhotos
-
-# figlet Step 8: You now need to manually upload the photos going to Google Photos
-# Remember we may need to enter password
-# figlet -k Google Photos, You may need to enter passwword
-
-# Move JPEG files to Google Photos
-# echo Moving JPEG photos to Google Photos
-# rclone move "$GOOGLE_PHOTOS_JPEG_UPLOAD_DIR" GooglePhotos:upload
