df2ad5d2ea1009db7344a6a34b28af749baada35
Frederick Price <rprice@pricemail.ca>
2024-04-08 21:15:53 -0400

    Update Neovim plugins
    Was a bit of a pain, might be a pain on other computers as well

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -443,8 +443,16 @@ return {
 		},
 	},
 
+    {
+        "vhyrro/luarocks.nvim",
+        priority = 1000,
+        config = true,
+    },
 	{
 		"nvim-neorg/neorg",
+		dependencies = { "luarocks.nvim" },
+	       lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+	       version = "*", -- Pin Neorg to the latest stable release
 		run = ":Neorg sync-parsers", -- This is the important bit!
 		config = function()
 			require("neorg").setup({
