7b3e1c5d2ae9ba7cafd9067b31bec9226586998b
Frederick Price <rprice@pricemail.ca>
2024-04-10 15:17:17 -0400

    Trying Fira Code for a bit

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -21,13 +21,15 @@ return {
 
 	-- font = wezterm.font_with_fallback({ "Iosevka", "Source Code Pro", "Source Code Variable", "PowerlineSymbols" }),
 	-- font = wezterm.font_with_fallback({ "Fira Code","Symbols Nerd Font Mono"}),
-	font = wezterm.font_with_fallback({ "Source Code Pro","Symbols Nerd Font Mono"}),
+	-- font = wezterm.font_with_fallback({ "Source Code Pro","Symbols Nerd Font Mono"}),
+	font = wezterm.font_with_fallback({ "Fira Code","Symbols Nerd Font Mono"}),
 	font_size = 10.0,
 	-- dpi = 96.0,
 	bold_brightens_ansi_colors = true,
 	-- font_dirs = {"/usr/share/fonts/TTF"},
 	font_shaper = "Harfbuzz",
-	harfbuzz_features = { "kern", "liga", "clig", "calt", "zero"},
+	-- harfbuzz_features = { "kern", "liga", "clig", "calt", "zero"},
+	harfbuzz_features = { "kern", "liga", "clig", "calt"},
 	scrollback_lines = 10000,
 
 	-- keys = {
