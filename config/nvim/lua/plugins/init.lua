140fdebe5143dc51a6e267a66ca90011b225b281
Frederick Price <rprice@pricemail.ca>
2024-04-20 11:06:03 -0400

    Update NVim Neogit to the nightly branch to get NVim 0.10 support

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -115,6 +115,7 @@ return {
 
 	{
 		"NeogitOrg/neogit",
+		branch = "nightly",
 		dependencies = {
 			"nvim-lua/plenary.nvim", -- required
 			"sindrets/diffview.nvim", -- optional - Diff integration
