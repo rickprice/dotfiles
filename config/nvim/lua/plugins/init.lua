2bc1dbb95aa1de63de4b96c89ee471d5e14983ea
Frederick Price <rprice@pricemail.ca>
2024-05-09 16:21:08 -0400

    Removed Neorg for a bit

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -456,47 +456,47 @@ return {
         priority = 1000,
         config = true,
     },
-	{
-		"nvim-neorg/neorg",
-		dependencies = { "luarocks.nvim" },
-	       lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-	       version = "*", -- Pin Neorg to the latest stable release
-		run = ":Neorg sync-parsers", -- This is the important bit!
-		config = function()
-			require("neorg").setup({
-				load = {
-					["core.defaults"] = {},
-					["core.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } },
-					["core.integrations.nvim-cmp"] = {},
-					["core.concealer"] = { config = { icon_preset = "diamond" } },
-					["core.keybinds"] = {
-						-- https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds.lua
-						config = {
-							default_keybinds = true,
-							neorg_leader = "<leader>",
-						},
-					},
-					["core.integrations.treesitter"] = {},
-					["core.esupports.metagen"] = { config = { type = "auto", update_date = true } },
-					["core.qol.toc"] = {},
-					["core.qol.todo_items"] = {},
-					["core.looking-glass"] = {},
-					["core.presenter"] = { config = { zen_mode = "zen-mode" } },
-					["core.export"] = {},
-					["core.export.markdown"] = { config = { extensions = "all" } },
-					["core.summary"] = {},
-					["core.tangle"] = { config = { report_on_empty = false } },
-					["core.ui.calendar"] = {},
-					["core.journal"] = {
-						config = {
-							strategy = "flat",
-							workspace = "Notes",
-						},
-					},
-				},
-			})
-		end,
-	},
+	-- {
+	-- 	"nvim-neorg/neorg",
+	-- 	dependencies = { "luarocks.nvim" },
+	--        lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+	--        version = "*", -- Pin Neorg to the latest stable release
+	-- 	run = ":Neorg sync-parsers", -- This is the important bit!
+	-- 	config = function()
+	-- 		require("neorg").setup({
+	-- 			load = {
+	-- 				["core.defaults"] = {},
+	-- 				["core.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } },
+	-- 				["core.integrations.nvim-cmp"] = {},
+	-- 				["core.concealer"] = { config = { icon_preset = "diamond" } },
+	-- 				["core.keybinds"] = {
+	-- 					-- https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds.lua
+	-- 					config = {
+	-- 						default_keybinds = true,
+	-- 						neorg_leader = "<leader>",
+	-- 					},
+	-- 				},
+	-- 				["core.integrations.treesitter"] = {},
+	-- 				["core.esupports.metagen"] = { config = { type = "auto", update_date = true } },
+	-- 				["core.qol.toc"] = {},
+	-- 				["core.qol.todo_items"] = {},
+	-- 				["core.looking-glass"] = {},
+	-- 				["core.presenter"] = { config = { zen_mode = "zen-mode" } },
+	-- 				["core.export"] = {},
+	-- 				["core.export.markdown"] = { config = { extensions = "all" } },
+	-- 				["core.summary"] = {},
+	-- 				["core.tangle"] = { config = { report_on_empty = false } },
+	-- 				["core.ui.calendar"] = {},
+	-- 				["core.journal"] = {
+	-- 					config = {
+	-- 						strategy = "flat",
+	-- 						workspace = "Notes",
+	-- 					},
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 	{
 		"folke/zen-mode.nvim",
 		opts = {
