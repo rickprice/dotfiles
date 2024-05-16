6c90ecee8f7cf94e856cb1d8031c9680d1a7ae28
Frederick Price <rprice@pricemail.ca>
2024-05-16 14:37:26 -0400

    Improve KloggLog so it ignores the filename on the end

--- local/bin/klogglog.sh
+++ local/bin/klogglog.sh
@@ -4,8 +4,10 @@ set -e -o pipefail
 
 RAWLOG=$(mktemp)
 LOG=$(mktemp)
+RAW_INCOMING_S3=$1
+COOKED_INCOMING_S3="${RAW_INCOMING_S3%/*}/logs.jsonl"
 
-aws s3 cp $1 $RAWLOG
+aws s3 cp $COOKED_INCOMING_S3 $RAWLOG
 jq -r .body.msg $RAWLOG > $LOG
 klogg $LOG
 rm -f $RAWLOG $LOG
