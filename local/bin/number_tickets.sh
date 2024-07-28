1ce1da005dd17652cd49f03ed16c34fe7a4c24b1
Frederick Price <rprice@pricemail.ca>
2024-07-27 23:51:21 -0400

    Update NumberTickets

--- local/bin/number_tickets.sh
+++ local/bin/number_tickets.sh
@@ -1,24 +1,28 @@
 #! /usr/bin/env bash
 
-typeset -i i END START
+typeset -i i
 
-let START=7
-let END=50
+# NUMBER_ARRAY=$(seq 1 64)
+NUMBER_ARRAY=(3 4 12 14 16 18)
+
+# NUP='1x4'
+NUP='2x5'
 
 FORMAT_STRING="%05d"
 
 TEMPORARY_DIRECTORY=$(mktemp -d)
 
+# echo "Number Array is:" $NUMBER_ARRAY
 echo "Temporary Directory is:" $TEMPORARY_DIRECTORY
 
 let i=$START
-while ((i<=END)); do
+
+for i in ${NUMBER_ARRAY[@]}; do
     TICKET_NUMBER=$(printf $FORMAT_STRING $i)
     # echo $TICKET_NUMBER
 
     sed "s/#TicKet#/$TICKET_NUMBER/g" EditedTicket.svg | inkscape --pipe --export-type=pdf --export-filename=$TEMPORARY_DIRECTORY/Ticket-$TICKET_NUMBER.pdf
 
-    let i++
 done
 
-pdfjam -o EditedTickets.pdf --nup 1x4 --landscape $TEMPORARY_DIRECTORY/*.pdf
+pdfjam -o EditedTickets.pdf --nup $NUP --landscape $TEMPORARY_DIRECTORY/*.pdf
