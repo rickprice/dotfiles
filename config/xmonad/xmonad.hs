205ae24907cb68e48a94ca54ed0f6cfa6f55a952
Frederick Price <rprice@pricemail.ca>
2024-10-02 22:36:46 -0400

    Add battery icon back in. Update XMonad submodules

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -219,7 +219,7 @@ myStartupHook  hostname= do
     spawnOnce "pamac-tray"
     spawnOnce "blueman-applet"
     spawn "killall volumeicon; sleep 15; volumeicon"
-    -- spawnOnce "cbatticon"
+    spawnOnce "cbatticon"
     spawnOnce "xfce4-power-manager"
     spawnOnce "killall udiskie; udiskie --tray"
     -- spawnOnce "qmidinet -n 6"
