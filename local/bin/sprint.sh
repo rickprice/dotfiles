d71afb06b9625d7a7447499a86eebcbf65ce9080
Frederick Price <rprice@pricemail.ca>
2025-01-07 14:02:43 -0500

    Explain how to fix things

--- local/bin/sprint.sh
+++ local/bin/sprint.sh
@@ -3,6 +3,15 @@
 # Prints out the current quarter, and the week in the quarter
 #
 
+# When this is broken, this seems to fix things:
+#
+# export SHELL=bash
+# bash
+# export PIPENV_VENV_IN_PROJECT=1
+# export PIPENV_PIPFILE=~/.dotfiles/bin/BE_Sprint_VENV/Pipfile
+# export SPRINTPROGRAM=~/.dotfiles/bin/BE_Sprint_VENV/current_BE_sprint.py
+# pipenv install
+
 #!/bin/bash
 
 set -e -o pipefail
