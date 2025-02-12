8cbfc6303cef3481fd608e4bb1921bb1ff0699d8
Frederick Price <rprice@pricemail.ca>
2025-02-12 14:59:17 -0500

    Setup4Monitors

--- local/bin/Setup4Monitors.sh
+++ local/bin/Setup4Monitors.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-xrandr --output eDP-1 --primary --mode 2560x1600 --pos 0x900 --rotate normal
+xrandr --dpi 120
+
+# xrandr --output eDP-1 --primary --mode 2560x1600 --pos 0x900 --rotate normal --scale .75x.75
+xrandr --output eDP-1 --primary --mode 2560x1600 --pos 0x900 --rotate normal --scale 1x1
 
 xrandr --output DP-1 --mode 2560x1080 --pos 2560x1080 --rotate normal 
 
 xrandr --output DP-2 --mode 1920x1080 --pos 2560x0 --rotate normal 
 
-xrandr --output DVI-I-2-1 --mode 1600x900 --pos 960x0 --rotate normal
+xrandr --output DVI-I-2-1 --mode 1600x900 --pos 960x0 --rotate normal5
