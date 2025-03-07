6b20d38be90329cbbbd94841c1c3dfb0f835dd32
Frederick Price <rprice@pricemail.ca>
2025-03-06 20:53:54 -0500

    Add in other bits that got lost

---
+++ config/nvim/lua/plugins/neorg.lua
@@ -0,0 +1,19 @@
+-- every spec file under config.plugins will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+	{
+		"vhyrro/luarocks.nvim",
+		priority = 1000,
+		config = true,
+	},
+	{
+		"nvim-neorg/neorg",
+		lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+		version = "*", -- Pin Neorg to the latest stable release
+		config = true,
+	},
+}
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,29 +1,29 @@
 return {
-    -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
--- treesitter, mason and typescript.nvim. So instead of the above, you can use:
---{ import = "lazyvim.plugins.extras.lang.typescript" },
+	-- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
+	-- treesitter, mason and typescript.nvim. So instead of the above, you can use:
+	--{ import = "lazyvim.plugins.extras.lang.typescript" },
 
--- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
--- would overwrite `ensure_installed` with the new value.
--- If you'd rather extend the default config, use the code below instead:
-{
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-        -- add tsx and treesitter
-        vim.list_extend(opts.ensure_installed, {
-            "bash",
-            "typescript",
-            "json",
-            "lua",
-            "markdown",
-            "markdown_inline",
-            "python",
-            "regex",
-            "vim",
-            "yaml",
-            "rust",
-            "haskell",
-        })
-        end,
-    },
+	-- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
+	-- would overwrite `ensure_installed` with the new value.
+	-- If you'd rather extend the default config, use the code below instead:
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			-- add tsx and treesitter
+			vim.list_extend(opts.ensure_installed, {
+				"bash",
+				"typescript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"regex",
+				"vim",
+				"yaml",
+				"rust",
+				"haskell",
+			})
+		end,
+	},
 }
