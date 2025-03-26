e569cdc19833d67a639fa35a0206ab9556e9abbe
Frederick Price <rprice@pricemail.ca>
2025-03-26 19:49:40 -0400

    Process Raw images doesn't geolocate right now
    Because Google no longer lets you download your location data, I'm using
    the GPS built into the camera for now.

--- local/bin/ProcessRawImages.sh
+++ local/bin/ProcessRawImages.sh
@@ -54,12 +54,12 @@ function process_photo_directory() {
     mkdir -p "$directory_location_data"
     cp -r $directory/* "$directory_location_data"
 
-    echo "Add location data $directory"
-    exiftool -v5 -Copyright="All rights reserved. $COMPANY" -CopyrightNotice="All rights reserved. $COMPANY" -Rights="All rights reserved" -Artist="$ARTIST" -geotag "$LOCATION_HISTORY_FILE_KML" '-geotime<${DateTimeOriginal}-05:00' . -api GeoMaxIntSecs=1800 -overwrite_original_in_place $directory_location_data
+    # echo "Add location data $directory"
+    # exiftool -v5 -Copyright="All rights reserved. $COMPANY" -CopyrightNotice="All rights reserved. $COMPANY" -Rights="All rights reserved" -Artist="$ARTIST" -geotag "$LOCATION_HISTORY_FILE_KML" '-geotime<${DateTimeOriginal}-05:00' . -api GeoMaxIntSecs=1800 -overwrite_original_in_place $directory_location_data
     echo "Organize Images $directory_location_data"
     exiftool '-filename<DateTimeOriginal' -d "$directory_location_data/%Y-%m-%d/%Y-%m-%d_%%-c%%f.%%e" $directory_location_data
 }
 
-setup_google_location_data
+# setup_google_location_data
 process_photo_directory "$WORKING_DIR/Incoming" "Pictures/CanonRawPhotos"
 # process_photo_directory "$WORKING_DIR/CanonSX540HS"  "Pictures/CanonSX540HS"
