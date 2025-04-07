1f5c68fb3eff1af8456258b955d6976945b54f7f
Frederick Price <rprice@pricemail.ca>
2025-04-06 23:03:17 -0400

    Change how we look for Tamara's mouse

--- local/bin/IsExternalLogitechMouseOn.sh
+++ local/bin/IsExternalLogitechMouseOn.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if solaar show | grep -q -e "Supports.*HID.*features:" ; then
-    echo "External Logitech mouse on"
+if xinput list --name-only | grep -q -e "Logitech M.+" ; then
+    echo "External Logitech mouse plugged in"
     exit 0
 else
-    echo "External Logitech mouse off"
+    echo "External Logitech mouse not plugged in"
     exit 1
 fi
