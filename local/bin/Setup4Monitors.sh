d52c9e93d095f906fc280d7ffa26bb14e62a0a64
Frederick Price <rprice@pricemail.ca>
2025-04-15 22:15:52 -0400

    Update monitor setup, again

--- local/bin/Setup4Monitors.sh
+++ local/bin/Setup4Monitors.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 xrandr --dpi 120
-xrandr --output eDP-1 --primary --mode 2560x1600 --pos 0x1080 --rotate normal --output DP-1 --mode 2560x1080 --pos 2560x1080 --rotate normal --output HDMI-1 --off --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 2560x0 --rotate normal --output DP-4 --off --output DP-5 --off --output DVI-I-2-1 --mode 1600x900 --pos 960x180 --rotate normal
+xrandr --output eDP-1 --primary --mode 2560x1600 --pos 0x1080 --rotate normal --output DP-1 --mode 2560x1080 --pos 2560x1080 --rotate normal --output HDMI-1 --off --output DP-2 --mode 1920x1080 --pos 2560x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off --output DVI-I-2-1 --mode 1600x900 --pos 960x180 --rotate normal
 
 ~/.config/autorandr/postswitch.d/10_setup_feh
