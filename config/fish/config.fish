b5aab0df0e409d78dd391c30681640e6fad1db75
Frederick Price <rprice@pricemail.ca>
2025-05-13 17:32:45 -0400

    Remove AS stuff from the path by default again

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -19,8 +19,8 @@ set -xg PATH "/home/fprice/.ghcup/bin:$PATH"
 set -xg PATH "/home/fprice/.local/ActiveState/StateTool/beta/bin:$PATH"
 # -- STOP ACTIVESTATE INSTALLATION
 # -- START ACTIVESTATE DEFAULT RUNTIME ENVIRONMENT
-set -xg PATH "/home/fprice/.cache/activestate/bin:$PATH"
-if test ! -z "$ACTIVESTATE_ACTIVATED"; test -f "$ACTIVESTATE_ACTIVATED/activestate.yaml"
-  echo "State Tool is operating on project $ACTIVESTATE_ACTIVATED_NAMESPACE, located at $ACTIVESTATE_ACTIVATED"
-end
+# set -xg PATH "/home/fprice/.cache/activestate/bin:$PATH"
+# if test ! -z "$ACTIVESTATE_ACTIVATED"; test -f "$ACTIVESTATE_ACTIVATED/activestate.yaml"
+#   echo "State Tool is operating on project $ACTIVESTATE_ACTIVATED_NAMESPACE, located at $ACTIVESTATE_ACTIVATED"
+# end
 # -- STOP ACTIVESTATE DEFAULT RUNTIME ENVIRONMENT
