6ec40ac69bb80bf93a7c6aaab8e3ab06c7724d44
Frederick Price <rprice@pricemail.ca>
2025-06-18 14:01:27 -0400

    Update Neovim plugins
    blink had a configuration issues with super-tab, so I reset it to
    default

--- config/nvim/lua/plugins/blink.cmp.lua
+++ config/nvim/lua/plugins/blink.cmp.lua
@@ -25,7 +25,7 @@ return {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'super-tab' },
+    keymap = { preset = 'default' },
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
@@ -34,7 +34,7 @@ return {
     },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = true } },
+    completion = { documentation = { auto_show = false } },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
