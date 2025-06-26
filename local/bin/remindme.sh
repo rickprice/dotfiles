81309a3564eab310f37d71cb6b355d2c58fc1fa3
Frederick Price <rprice@pricemail.ca>
2025-06-26 12:37:43 -0400

    Update remindme script a bit

--- local/bin/remindme.sh
+++ local/bin/remindme.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # Usage: remindme -t "3m 1s" -m "I have to do homework"
+# Usage: remindme "message" (sends immediately)
+
+# If no flags provided, treat first argument as immediate message
+if [[ $# -eq 1 && $1 != -* ]]; then
+    notify-send -a Reminder "$1"
+    exit 0
+fi
+
 while getopts "t:m:" optname; do
 case $optname in
 t)
@@ -10,4 +18,9 @@ message=${OPTARG}
 ;;
 esac
 done
-coproc (sleep $time && notify-send -a Reminder "$message")
+
+if [ -z "$time" ]; then
+    notify-send -a Reminder "$message"
+else
+    coproc (sleep $time && notify-send -a Reminder "$message")
+fi
