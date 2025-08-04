97066e8e13b7fe77ecc958512fd79f392188edc3
Frederick Price <rprice@pricemail.ca>
2025-08-04 11:33:45 -0400

    Fix bug in my xmonad.hs

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -261,7 +261,7 @@ myStartupHook  hostname= do
     spawn "killall volumeicon; sleep 15; volumeicon"
     spawnOnce "xfce4-power-manager"
     spawnOnce "killall udiskie; udiskie --tray"
-    spawnOnce "syncthing servec"
+    spawnOnce "syncthing serve"
     spawn myFixScreens
     spawn myFixLogitechMouse
     
