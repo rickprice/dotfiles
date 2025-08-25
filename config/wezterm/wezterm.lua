252895ab1974242d3e004352aad5fd45b1a12294
Frederick Price <rprice@pricemail.ca>
2025-08-25 16:06:09 -0400

    Use white background on Mac

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,12 +1,40 @@
 local wezterm = require("wezterm")
-return {
+
+local config = {
 	enable_tab_bar = false,
 	-- color_scheme="Adventure",
 	-- color_scheme="Jellybeans",
 	-- color_scheme="BuiltinDark",
 
-	-- Based off Jellybeans, but with pure black background
-	colors = {
+	font = wezterm.font_with_fallback({ "Fira Code","Symbols Nerd Font Mono"}),
+	font_size = 10.0,
+	-- dpi = 96.0,
+	bold_brightens_ansi_colors = true,
+	-- font_dirs = {"/usr/share/fonts/TTF"},
+	font_shaper = "Harfbuzz",
+	-- harfbuzz_features = { "kern", "liga", "clig", "calt", "zero"},
+	harfbuzz_features = { "kern", "liga", "clig", "calt"},
+	scrollback_lines = 10000,
+}
+
+-- Platform-specific colors
+if wezterm.target_triple:find("darwin") then
+	-- macOS - white background
+	config.colors = {
+		foreground = "#333333",
+		background = "#ffffff",
+		cursor_bg = "#ff6600",
+		cursor_border = "#ff6600",
+		cursor_fg = "#ffffff",
+		selection_bg = "#b3d4fc",
+		selection_fg = "#000000",
+
+		ansi = { "#000000", "#d70000", "#00d700", "#d7af00", "#0087d7", "#d700d7", "#00d7af", "#d7d7d7" },
+		brights = { "#808080", "#ff5f5f", "#5fff5f", "#ffff5f", "#5f87ff", "#ff5fff", "#5fffff", "#ffffff" },
+	}
+else
+	-- Other platforms - black background (original Jellybeans-based)
+	config.colors = {
 		foreground = "#dedede",
 		background = "black",
 		cursor_bg = "#ffa560",
@@ -17,74 +45,31 @@ return {
 
 		ansi = { "#929292", "#e27373", "#94b979", "#ffba7b", "#97bedc", "#e1c0fa", "#00988e", "#dedede" },
 		brights = { "#bdbdbd", "#ffa1a1", "#bddeab", "#ffdca0", "#b1d8f6", "#fbdaff", "#1ab2a8", "#ffffff" },
-	},
+	}
+end
 
-	-- font = wezterm.font_with_fallback({ "Iosevka", "Source Code Pro", "Source Code Variable", "PowerlineSymbols" }),
-	-- font = wezterm.font_with_fallback({ "Fira Code","Symbols Nerd Font Mono"}),
-	-- font = wezterm.font_with_fallback({ "Source Code Pro","Symbols Nerd Font Mono"}),
-	font = wezterm.font_with_fallback({ "Fira Code","Symbols Nerd Font Mono"}),
-	font_size = 10.0,
-	-- dpi = 96.0,
-	bold_brightens_ansi_colors = true,
-	-- font_dirs = {"/usr/share/fonts/TTF"},
-	font_shaper = "Harfbuzz",
-	-- harfbuzz_features = { "kern", "liga", "clig", "calt", "zero"},
-	harfbuzz_features = { "kern", "liga", "clig", "calt"},
-	scrollback_lines = 10000,
-
-	-- keys = {
-	--   -- copy to clipboard and primary selection
-	--   { key = "C", mods = "SHIFT|ALT", action = wezterm.action({ CopyTo = "ClipboardAndPrimarySelection" }) },
-	--   -- paste from the clipboard
-	--   { key = "V", mods = "SHIFT|ALT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
-	-- },
-	-- mouse_bindings = {
-	--   -- Bind 'Up' event of CTRL-Click to open hyperlinks
-	--   {
-	--     event = { Up = { streak = 1, button = "Left" } },
-	--     mods = "CTRL",
-	--     action = "OpenLinkAtMouseCursor",
-	--   },
-	--   -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
-	--   {
-	--     event = { Down = { streak = 1, button = "Left" } },
-	--     mods = "CTRL",
-	--     action = "Nop",
-	--   },
-	-- -- Bind 'Up' event of Double-Click to copy to clipboard and selection
-	-- {
-	--   event = { Up = { streak = 2, button = "Left" } },
-	--   mods = "",
-	--   action = "ClipboardAndPrimarySelection",
-	-- },
-	-- -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
-	-- {
-	--   event = { Down = { streak = 2, button = "Left" } },
-	--   mods = "",
-	--   action = "Nop",
-	-- },
-	-- },
-
-	wezterm.on("user-var-changed", function(window, pane, name, value)
-		local overrides = window:get_config_overrides() or {}
-		if name == "ZEN_MODE" then
-			local incremental = value:find("+")
-			local number_value = tonumber(value)
-			if incremental ~= nil then
-				while number_value > 0 do
-					window:perform_action(wezterm.action.IncreaseFontSize, pane)
-					number_value = number_value - 1
-				end
-				overrides.enable_tab_bar = false
-			elseif number_value < 0 then
-				window:perform_action(wezterm.action.ResetFontSize, pane)
-				overrides.font_size = nil
-				overrides.enable_tab_bar = true
-			else
-				overrides.font_size = number_value
-				overrides.enable_tab_bar = false
+-- Event handler for zen mode
+wezterm.on("user-var-changed", function(window, pane, name, value)
+	local overrides = window:get_config_overrides() or {}
+	if name == "ZEN_MODE" then
+		local incremental = value:find("+")
+		local number_value = tonumber(value)
+		if incremental ~= nil then
+			while number_value > 0 do
+				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+				number_value = number_value - 1
 			end
+			overrides.enable_tab_bar = false
+		elseif number_value < 0 then
+			window:perform_action(wezterm.action.ResetFontSize, pane)
+			overrides.font_size = nil
+			overrides.enable_tab_bar = true
+		else
+			overrides.font_size = number_value
+			overrides.enable_tab_bar = false
 		end
-		window:set_config_overrides(overrides)
-	end),
-}
+	end
+	window:set_config_overrides(overrides)
+end)
+
+return config
