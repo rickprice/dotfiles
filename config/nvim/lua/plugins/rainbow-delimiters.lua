7ba8653c82e1eb01507562f43762207f2c72402d
Frederick Price <fprice@pricemail.ca>
2025-10-01 10:15:00 -0400

    Remove empty line that was warned about

--- config/nvim/lua/plugins/rainbow-delimiters.lua
+++ config/nvim/lua/plugins/rainbow-delimiters.lua
@@ -2,7 +2,6 @@ return {
   "HiPhish/rainbow-delimiters.nvim",
   config = function()
     local rainbow_delimiters = require 'rainbow-delimiters'
-    
     vim.g.rainbow_delimiters = {
       strategy = {
         [''] = rainbow_delimiters.strategy['global'],
