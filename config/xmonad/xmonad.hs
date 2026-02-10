669c0948eb697cc772501624b78cb279d0d90873
Frederick Price <fprice@pricemail.ca>
2026-02-10 09:49:47 -0500

    Run EasyEffects in the background

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -263,7 +263,7 @@ myStartupHook  hostname= do
     spawn "killall pasystray; sleep 15; pasystray"
     spawnOnce "xfce4-power-manager"
     spawnOnce "killall udiskie; udiskie --tray"
-    spawnOnce "easyeffects"
+    spawnOnce "easyeffects --service-mode --hide-window"
     spawn myFixScreens
     spawn myFixLogitechMouse
     
